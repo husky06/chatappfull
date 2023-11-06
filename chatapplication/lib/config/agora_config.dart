3e7d026e21256290d9d5539c3824f5fb4c9dc17e
husly06 <thanhcuong061002@gmail.com>
2023-11-06 21:04:00 +0700

    Hoan thanh

---
+++ chatapplication/lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '1f24a1208c91411e93d9852dd2326b99';
+  static String appcertificate = '03abd78adea448b59c35bac073e862ba';
+}
